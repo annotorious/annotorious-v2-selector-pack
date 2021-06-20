7135da32d7009b63030b7010c14f734052b1f53e
Rainer Simon <aboutgeo@gmail.com>
2021-06-20 07:34:14 +0200

    Initial import: moved circle tool selector

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import RubberbandCircleTool from './circle/RubberbandCircleTool';
+
+const SelectorPack = (anno, config) => {
+
+  anno.addDrawingTool(RubberbandCircleTool);
+  
+}
+
+export default SelectorPack;

f5398e5589414b725fc537870ef598a5f76fd441
Hero Solutions <info@herosolutions.be>
2021-06-20 15:55:06 +0200

    Registers ellipse tool

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import RubberbandCircleTool from './circle/RubberbandCircleTool';
+import RubberbandEllipseTool from './circle/RubberbandEllipseTool';
 
 const SelectorPack = (anno, config) => {
 
   anno.addDrawingTool(RubberbandCircleTool);
+  anno.addDrawingTool(RubberbandEllipseTool);
   
 }
 

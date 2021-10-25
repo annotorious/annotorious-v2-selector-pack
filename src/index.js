7d01df648278f4510e7ef406f57b263af2d8d8e8
Rainer Simon <aboutgeo@gmail.com>
2021-10-25 12:30:59 +0200

    Made selection of tools configurable

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import RubberbandCircleTool from './circle/RubberbandCircleTool';
 import RubberbandEllipseTool from './ellipse/RubberbandEllipseTool';
 import RubberbandFreehandTool from './freehand/RubberbandFreehandTool';
 
+const ALL_TOOLS = new Set([
+  'circle',
+  'ellipse',
+  'freehand'
+]);
+
 const SelectorPack = (anno, config) => {
 
-  anno.addDrawingTool(RubberbandCircleTool);
-  anno.addDrawingTool(RubberbandEllipseTool);
-  anno.addDrawingTool(RubberbandFreehandTool);
+  // Add configured tools, or all
+  const useTools = config?.tools ? 
+    new Set(config.tools.map(t => t.toLowerCase())) : ALL_TOOLS;
+
+  if (useTools.has('circle'))
+    anno.addDrawingTool(RubberbandCircleTool);
+
+  if (useTools.has('ellipse'))
+    anno.addDrawingTool(RubberbandEllipseTool);
+  
+  if (useTools.has('freehand'))
+    anno.addDrawingTool(RubberbandFreehandTool);
   
 }
 
